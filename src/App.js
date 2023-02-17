c76b7e5527b10335f4c75150b7a42ed9d243f0fe
webhasan <webhasan24@gmail.com>
2023-02-17 12:00:18 +0600

    Theme setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [theme, colorMode] = useMode();
+
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <div className="app">
+                    <div className="content">
+                        
+                    </div>
+                </div>
+            </ThemeProvider>    
+        </ColorModeContext.Provider>
+    )
 }
 
 export default App;
---
+++ src/scens/dashboard/index.js
@@ -0,0 +1,5 @@
+const Dashboard = () => {
+    return <h1>This is dashboard</h1>
+}
+
+export default Dashboard;
